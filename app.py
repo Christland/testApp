53ee25045de727c4524f098d1daa1b439efe2d06
Christland <acquayechristabel@gmail.com>
2019-08-27 17:37:19 +0000

    Added new html file

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ app = Flask("__name__")
 @app.route("/")
 def index():
     return render_template("test1.html")
-    
+
+@app.route("/second")
+def second():
+      return render_template("test.html")
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
